ec1b5f4c33cac09a4ee6e75884997eb3ea1f0f25
Shamayal Syed <shamayalsyed9@gmail.com>
2025-01-04 01:41:48 -0500

    added function to start timer

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-let timer;
-let minutes = 25;
-let seconds = 0;
-
 function startTimer() {
   timer = setInterval(updateTimer, 1000);
+}
+
+const start = document.getElementById("start-btn");
+const pause = document.getElementById("pause-btn");
+const restart = document.getElementById("restart-btn");
+
+let timer = null;
+let time = 25 * 60;
+
+const updateTimeDisplay = () => {
+  let minutes = Math.floor(time/60);
+  let seconds = time % 60;
+  displat.textContent = `${minutes.toString().padStart('2', '0')}`
+}
+
+// function to start the timer
+const startTimer = () => {
+  if (!timer) {
+    timer = setInterval(() => {
+      time --;
+      updateTimeDisplay();
+      if (time === 0) {
+        timer = null;
+        alert('Time is Up!');
+      }
+    }, 1000);
+  }
 }
