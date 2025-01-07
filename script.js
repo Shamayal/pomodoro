dc95df7f7bbdeabfeb633689b5d361d3c4586a3e
Shamayal Syed <shamayalsyed9@gmail.com>
2025-01-06 22:40:49 -0500

    created 3 new variables to start, pause, and restart the timer

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
+// set timer variable
+let timerInterval;
+// 25 minutes in seconds
+let timer = 25 * 60; 
+// initially paused
+let isPaused = true;
+
+// start the timer
 function startTimer() {
-  timer = setInterval(updateTimer, 1000);
+
 }
 
-const start = document.getElementById("start-btn");
-const pause = document.getElementById("pause-btn");
-const restart = document.getElementById("restart-btn");
+// pause the timer
+function pauseTimer() {
 
-let timer = null;
-let time = 25 * 60;
-
-const updateTimeDisplay = () => {
-  let minutes = Math.floor(time/60);
-  let seconds = time % 60;
-  displat.textContent = `${minutes.toString().padStart('2', '0')}`
 }
 
-// function to start the timer
-const startTimer = () => {
-  if (!timer) {
-    timer = setInterval(() => {
-      time --;
-      updateTimeDisplay();
-      if (time === 0) {
-        timer = null;
-        alert('Time is Up!');
-      }
-    }, 1000);
-  }
+// restart the timer
+function restartTimer() {
+
 }
