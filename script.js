446ff82a71397a929dbba7a3082075ebf0cfea7a
Shamayal Syed <shamayalsyed9@gmail.com>
2025-09-06 16:52:23 -0400

    Added variables for buttons

---
+++ script.js
@@ -0,0 +1,6 @@
+const settings = document.getElementById("settings");
+const pomodoro = document.getElementById("pomodoro");
+const shortBreak = document.getElementById("short-break");
+const longBreak = document.getElementById("long-bread");
+const start = document.getElementById("start");
+//const pause = document.getElementById("start");
