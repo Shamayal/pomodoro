b2cbd4872a08f2a12934a2a71f3564d3d5a78e2d
Shamayal Syed <shamayalsyed9@gmail.com>
2025-09-06 22:39:40 -0400

    Added a function to toggle between Start and Pause for the timer

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const pomodoro = document.getElementById("pomodoro");
 const shortBreak = document.getElementById("short-break");
 const longBreak = document.getElementById("long-break");
 const start = document.getElementById("start");
-//const pause = document.getElementById("start");
+const pause = document.getElementById("start");
 const timerDisplay = document.querySelector(".timer");
 
 let countdown;
@@ -11,27 +11,35 @@ let pomodoroTime = 25;
 let timeLeft = pomodoroTime * 60;
 let timerRunning = false;
 
-function startTimer() {
+function toggleTimer() {
   if (!timerRunning) {
     timerRunning = true;
     start.textContent = "Pause";
+
+    countdown = setInterval(() => {
+      if (timeLeft <= 0) {
+        clearInterval(countdown);
+        timerDisplay.textContent = "00:00";
+        alert("Time's up!");
+        timerRunning = false;
+        start.textContent = "Start";
+        return;
+      }
+
+      timeLeft--;
+
+      const minutes = Math.floor(timeLeft / 60);
+      const seconds = timeLeft % 60;
+
+      timerDisplay.textContent = `${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    }, 1000);
+  } else {
+    clearInterval(countdown);
+    timerRunning = false;
+    start.textContent = "Start";
   }
-  countdown = setInterval(() => {
-    if (timeLeft <= 0) {
-      timerDisplay.textContent = "00:00";
-      alert("Time's up!");
-      return;
-    }
-    timeLeft --;
-
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-
-    timerDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
-      .toString()
-      .padStart(2, "0")}`;
-  }, 1000);
 }
 
-start.addEventListener("click", startTimer);
-//pause.addEventListener("click", stopTimer);
+start.addEventListener("click", toggleTimer);
