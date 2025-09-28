a079063c3a8f95bcd161c76a60401d716408e89d
Shamayal Syed <shamayalsyed9@gmail.com>
2025-09-28 17:07:34 -0400

    Moved DOM elements to a new file

---
+++ js/dom.js
@@ -0,0 +1,26 @@
+export const elements = {
+  // Start
+  start: document.getElementById("start"),
+  // Mode Buttons
+  pomodoro: document.getElementById("pomodoro"),
+  shortBreak: document.getElementById("short-break"),
+  longBreak: document.getElementById("long-break"),
+  // Mode Lengths
+  inputs: {
+    pomodoroInput: document.getElementById("pomodoro-length"), 
+    shortInput: document.getElementById("short-length"),
+    longInput: document.getElementById("long-length"),
+  },
+  // Time Displays
+  timerDisplay: document.querySelector(".timer"),
+  progressBar: document.getElementById("progress-bar"),
+  // Settings
+  settingsButton: document.getElementById("settings"),
+  settingsModal: document.getElementById("settings-modal"),
+  closeSettingsBtn: document.getElementById("close-settings"),
+  modalBackdrop: document.getElementById("modal-backdrop"),
+  // Dark Mode
+  darkModeToggleBtn: document.getElementById("dark-mode-toggle"),
+  // Sound
+  soundToggleBtn: document.getElementById("dark-mode-toggle")
+}
